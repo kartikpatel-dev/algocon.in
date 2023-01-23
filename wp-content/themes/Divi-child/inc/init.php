eca412c9f50ec9736119e28fa160e6d4f049b56e
kartikdholariya7600 <kartik.dholariya@nextgentm.com>
2023-01-23 18:37:56 +0530

    changes

--- wp-content/themes/Divi-child/inc/init.php
+++ wp-content/themes/Divi-child/inc/init.php
@@ -2,5 +2,6 @@
 // require get_template_directory() . '-child/inc/article-cpt.php';
 
 require get_template_directory() . '-child/inc/product-cpt.php';
+require get_template_directory() . '-child/inc/products/home-product-section.php';
 
 require get_template_directory() . '-child/inc/service-cpt.php';
